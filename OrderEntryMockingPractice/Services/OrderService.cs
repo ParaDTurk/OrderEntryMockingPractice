376220e582a804ff92d583661cdfc2647f04bed5
David Turk <dturk@paraport.com>
2016-04-26 13:17:23 -0700

    Creating substitutes in tests

--- OrderEntryMockingPractice/Services/OrderService.cs
+++ OrderEntryMockingPractice/Services/OrderService.cs
@@ -4,6 +4,25 @@ namespace OrderEntryMockingPractice.Services
 {
     public class OrderService
     {
+        private ICustomerRepository _customerRepository;
+        private IOrderFulfillmentService _orderFulfillmentService;
+        private IEmailService _emailService;
+        private IProductRepository _productRepository;
+        private ITaxRateService _taxRateService;
+
+        public OrderService (ICustomerRepository customerRepository, 
+                             IEmailService emailService, 
+                             IOrderFulfillmentService orderFulfillmentService, 
+                             IProductRepository productRepository, 
+                             ITaxRateService taxRateService)
+        {
+            _customerRepository = customerRepository;
+            _orderFulfillmentService = orderFulfillmentService;
+            _emailService = emailService;
+            _productRepository = productRepository;
+            _taxRateService = taxRateService;
+        }
+
         public OrderSummary PlaceOrder(Order order)
         {
             return null;
--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,30 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿using NUnit;
+using NSubstitute;
+using NUnit.Framework;
+using OrderEntryMockingPractice.Models;
+using OrderEntryMockingPractice.Services;
+
+namespace OrderEntryMockingPracticeTests
 {
+    [TestFixture]
     public class OrderServiceTests
     {
+        private ICustomerRepository _fakeCustomerRepository;
+        private IEmailService _fakeEmailService;
+        private OrderService _orderService;
+        private IOrderFulfillmentService _fakeOrderFulfillmentService;
+        private IProductRepository _fakeProductRepository;
+        private ITaxRateService _fakeTaxRateService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeCustomerRepository = Substitute.For <ICustomerRepository>();
+            _fakeEmailService = Substitute.For<IEmailService>();
+            _fakeOrderFulfillmentService = Substitute.For<IOrderFulfillmentService>();
+            _fakeProductRepository = Substitute.For<IProductRepository>();
+            _fakeTaxRateService = Substitute.For<ITaxRateService>();
+            _orderService = new OrderService(_fakeCustomerRepository, _fakeEmailService, _fakeOrderFulfillmentService, _fakeProductRepository, _fakeTaxRateService);
+        }
     }
 }
